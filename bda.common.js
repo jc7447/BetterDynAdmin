f08eca99e9cdbfc12795eb3674288d693b65f0f7
troussej <trousset.joel@gmail.com>
2016-06-29 12:25:15 +0200

    fix typeahead multiples

--- bda.common.js
+++ bda.common.js
@@ -260,17 +260,6 @@ try{
     return sorted;
   };
 
-  this.sortUnique = function (arr) {
-    if (arr.length === 0) return arr;
-    arr = arr.sort(function (a, b) { return a*1 - b*1; });
-    var ret = [arr[0]];
-    for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
-        if (arr[i-1] !== arr[i]) {
-            ret.push(arr[i]);
-        }
-    }
-    return ret;
-  };
 
   this.copyToClipboard = function (text)
   {
