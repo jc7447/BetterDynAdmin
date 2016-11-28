a22b1477a1b2fcdede17ab28c3e831a3c979da18
troussej <trousset.joel@gmail.com>
2016-11-28 17:53:25 +0100

    #103 save current editor/console content in localstorage

--- bda.common.js
+++ bda.common.js
@@ -2,7 +2,7 @@
 
 try {
 
-  var isLoggingTrace = true;
+  var isLoggingTrace = false;
   var xmlDefinitionCacheTimeout = 1200; // 20min
 
   // ----- Standard Javascript override -----
@@ -439,6 +439,25 @@ try {
     $elems.detach().appendTo($this);
     return this;
   }
+
+  // Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
 
   /*
 
