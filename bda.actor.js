7a86ac1f45b5d3a71cdb04137b92a3e8650b3040
Joel Trousset <external.z08jtrou@decathlon.com>
2017-09-04 15:33:41 +0200

    actor xml preview #119

--- bda.actor.js
+++ bda.actor.js
@@ -1,23 +1,27 @@
 (function($) {
   "use strict";
   var BDA_ACTOR = {
-    build : function()
-    {
+    build: function() {
       console.time("bdaActor");
       BDA_ACTOR.isActorChainPage = BDA_ACTOR.isActorChainPageFct();
-
+      BDA_ACTOR.isActorPage = BDA_ACTOR.isActorPageFct();
       if (BDA_ACTOR.isActorChainPage)
-          BDA_ACTOR.createActorCaller();
+        BDA_ACTOR.createActorCaller();
+      if (BDA_ACTOR.isActorPage) {
+        BDA_ACTOR.completeActorPage();
+      }
       console.timeEnd("bdaActor");
     },
 
-    isActorChainPageFct : function()
-    {
-        return $("h2:contains('Actor Chain:')").length == 1 && document.URL.indexOf('chainId=') != -1;
+    isActorChainPageFct: function() {
+      return $("h2:contains('Actor Chain:')").length == 1 && document.URL.indexOf('chainId=') != -1;
+    },
+    isActorPageFct: function() {
+      return $('h2:contains("Actor Chains")').length == 1;
     },
 
-    createActorCaller : function()
-    {
+
+    createActorCaller: function() {
       var componentPathName = getCurrentComponentPath();
       var tableActor = $('table:first');
       var tableActorHeaderRow = tableActor.find('tr:first');
@@ -27,57 +31,99 @@
       var tableActorDataRowCells = tableActorDataRow.find('td');
       var actorChainIdValue = $("h2:contains('Actor Chain:')").text().replace("Actor Chain: ", "");
 
-      var inputsHeader = tableActorHeaderColumns.filter(function(index, element){
+      var inputsHeader = tableActorHeaderColumns.filter(function(index, element) {
         return $(element).text() === "Inputs";
       });
       var inputsIndex = $(inputsHeader).index();
       var tableInputs = $(tableActorDataRow.children().get(inputsIndex)).children().get(0);
       var inputs = [];
-      if(tableInputs !== undefined)
-      {
-          var inputRows = $(tableInputs).find('tr');
-          var inputsSize = inputRows.length;
-          for(var i = 1; i < inputsSize; i++)
-          {
-              var inputRow = $(inputRows.get(i));
-              var name = $(inputRow.children().get(0));
-              var value = $(inputRow.children().get(1));
-              var isNucleus = value.text().indexOf("nucleus") != -1;
-              if(!isNucleus)
-              {
-                  inputs.push(name.text());
-              }
+      if (tableInputs !== undefined) {
+        var inputRows = $(tableInputs).find('tr');
+        var inputsSize = inputRows.length;
+        for (var i = 1; i < inputsSize; i++) {
+          var inputRow = $(inputRows.get(i));
+          var name = $(inputRow.children().get(0));
+          var value = $(inputRow.children().get(1));
+          var isNucleus = value.text().indexOf("nucleus") != -1;
+          if (!isNucleus) {
+            inputs.push(name.text());
           }
+        }
       }
       var inputsHTML = "";
-      for(var input in inputs)
-      {
-          inputsHTML += inputs[input] + " <textarea name='" + inputs[input] + "'></textarea><br />";
+      for (var input in inputs) {
+        inputsHTML += inputs[input] + " <textarea name='" + inputs[input] + "'></textarea><br />";
       }
       var url = window.location.origin + '/rest/model' + componentPathName + actorChainIdValue;
       logTrace(url);
-      var actorChainCallHtml = "<div id='actorChainCall' border>"
-          + "<h2>Call actor</h2>"
-          + "<a href='javascript:void(0)' id='copyChainUrl'>Copy URL to clipboard</a>";
-          if (inputs.length > 0)
-            actorChainCallHtml += "<br />Post parameters are " + inputs + "<br />";
-          actorChainCallHtml += "<form method='POST' action='/rest/model" + componentPathName + actorChainIdValue + "'>"
-          + inputsHTML
-          + "<button type='submit'>Call <i class='fa fa-play fa-x'></button>"
-          + "</form></div>";
+      var actorChainCallHtml = "<div id='actorChainCall' border>" + "<h2>Call actor</h2>" + "<a href='javascript:void(0)' id='copyChainUrl'>Copy URL to clipboard</a>";
+      if (inputs.length > 0)
+        actorChainCallHtml += "<br />Post parameters are " + inputs + "<br />";
+      actorChainCallHtml += "<form method='POST' action='/rest/model" + componentPathName + actorChainIdValue + "'>" + inputsHTML + "<button type='submit'>Call <i class='fa fa-play fa-x'></button>" + "</form></div>";
 
       tableActor.after(actorChainCallHtml);
-      $("#copyChainUrl").click(function(){
+      $("#copyChainUrl").click(function() {
         copyToClipboard(url);
       });
+    },
+
+    completeActorPage: function() {
+      BDA_ACTOR.$ACTOR_LIST = $('h2:contains("Actor Chains")').next(); // ul
+      processRepositoryXmlDef('definitionFile', (xmlDef) => {
+        BDA_ACTOR.$xmlDef = xmlDef;
+        BDA_ACTOR.$ACTOR_LIST.find('li').each(function(index, elem) {
+          var $elem = $(elem);
+          var actorName = $elem.find('a').text();
+          let actorDef = '';
+          var id = "xml_" + actorName;
+          let codeBlock = BDA_ACTOR.addActorXml(actorName, id, $elem);
+
+          $('<button class="showActorXml"><i class="fa fa-code link"></i></button>')
+            .on('click', (event) => {
+              codeBlock.toggleClass('hidden');
+              // console.log('click', codeBlock.attr());
+            })
+            .appendTo($elem);
+          // $elem.attr("id", chainName);
+          // $elem.find("td:eq(7)").click(function() {
+          //   var $td = $(this);
+          //   if ($td.hasClass("open")) {
+          //     $td.removeClass("open");
+          //     BDA_PIPELINE.hidePipelineXml(chainName);
+          //   } else {
+          //     $td.addClass("open");
+          //     BDA_PIPELINE.showPipelineXml(chainName, $elem.hasClass("odd"));
+          //   }
+          // });
+
+          // $elem.find("td:eq(8)").click(function() {
+          //   // Redset direction
+          //   BDA_PIPELINE.options.layout.hierarchical.direction = "LR";
+          //   BDA_PIPELINE.showPipelineGraph(chainName);
+          // });
+        });
+      })
+    },
+
+    addActorXml: function(actorName, id, $elem) {
+      console.log("Show actor XML for chain : " + actorName);
+
+      var xml = BDA_ACTOR.$xmlDef.find("actor-chain[id=" + actorName + "]")[0].outerHTML;
+      let wrapper = $("<div class='hidden' id='" + id + "'><pre></pre></div>");
+      var $codeBlock = wrapper.insertAfter($elem)
+        .find("pre")
+        .text(xml);
+      // if (isOdd)
+      //   $("#" + trId).addClass("odd");
+      highlightAndIndentXml($codeBlock);
+      return wrapper;
     },
 
   };
   // Reference to BDA
   var BDA;
   // Jquery plugin creation
-  $.fn.bdaActor = function(pBDA)
-   {
+  $.fn.bdaActor = function(pBDA) {
     console.log('Init plugin {0}'.format('bdaActor'));
     //settings = $.extend({}, defaults, options);
     BDA = pBDA;
